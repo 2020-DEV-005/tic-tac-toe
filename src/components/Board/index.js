6691cc4f477f4414e7740330bfd61eee38411b5f
Chandrashekar Reddy Muthumul <782902@TCSNSN.COM>
2020-02-13 19:29:22 +0530

    Box filled with player X for the first click on the board

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -7,6 +7,7 @@ class Board extends Component {
     constructor(props){
         super(props);
         this.state = {
+            filledBoxes: []
         };
     }
 
@@ -14,9 +15,21 @@ class Board extends Component {
         const totalBoxes = AppConst.TOTAL_BOXES;
         let boxList = []
         for(let i=0; i<totalBoxes; i++) {
-            boxList.push(<li key={i}><Box /></li>);
+            boxList.push(<li key={i}><Box onClick={this.fillTheBox.bind(this, i)} value={this.getFilledValue(i)} /></li>);
         }
         return boxList;
+    }
+    
+    getFilledValue = (boxIndex) => {
+        return this.state.filledBoxes[boxIndex] || "";
+    }
+
+    fillTheBox = (boxIndex) => {
+        let filledBoxes = this.state.filledBoxes;
+        filledBoxes[boxIndex] = AppConst.PLAYER_X_NAME;
+        this.setState(() => ({
+            filledBoxes: filledBoxes
+        }));
     }
 
     render = () => {
--- src/components/Box/index.test.js
+++ src/components/Box/index.test.js
@@ -3,10 +3,17 @@ import {shallow} from 'enzyme';
 import Box from '../Box/';
 
 describe("<Box /> component", () => {
-  let wrapper = shallow(<Box />);
+  let wrapper;
+  beforeEach( () => {
+    wrapper = shallow(<Box onClick={jest.fn()} />);
+  });
 
   it("Should have button element", () => {
     expect(wrapper.find("button").length).toEqual(1);
+  });
+
+  it("Should have the onClick prop of type function", () => {
+    expect(wrapper.props().onClick.constructor.name).toEqual("Function");
   });
 
 });
